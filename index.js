ef8b18f607828aa437ed51e311bf46d76a7e3ae3
Shifat ullah <shifatullah128@gmail.com>
2023-03-10 15:48:39 +0600

    1st commit

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 document.getElementById('kitkat-buy-btn').addEventListener('click', function(){
    const kitket = document.getElementById('kitkat-quantity').value;
    const price = parseFloat(kitket * 200);
-   add('chocolate', price);
+   add ('chocolate', price);
 })
 
 document.getElementById('rose-buy-btn').addEventListener('click', function(){
     const rose = document.getElementById('rose-quantity').value;
     const roseprice = parseFloat(rose * 100);
-    add('rose', roseprice);
+    add ('rose', roseprice);
+   
  })
 
  document.getElementById('diary-buy-btn').addEventListener('click', function(){
     const diary = document.getElementById('diary-quantitydiary-quantity').value;
     const diaryprice = parseFloat(diary * 100);
-    add('diary', diaryprice )
+    add ("diary", diaryprice);
  })
 
 
- function add(id,value){
-    document.getElementById(id).innerText=value;
+ function add (id ,value){
+   document.getElementById(id, value).innerText = value;
+ 
  }
+
+
